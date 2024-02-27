47cb5305c590ca676c826e0c5d9c8f825e9585ce
Carlos E Valencia <choterifa@gmail.com>
2024-02-27 00:55:36 -0600

    acutalizado

--- app.py
+++ app.py
@@ -4,10 +4,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-
-@app.route('/') #ruta principal
+@app.route('/')  # ruta principal
 def index():
     return render_template('base.html')
+
+
+@app.route('/home')  # ruta principal
+def home():
+    return render_template('home.html')
 
 
 @app.route('/login')  # ruta principal
