367c2aba14728d00fc627bccbc16dcc4d9a631b9
Carlos E Valencia <choterifa@gmail.com>
2024-02-27 00:20:23 -0600

    agregado htmls

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')  # ruta principal
+def index():
+    return render_template('base.html')
+
+
+@app.route('/login')  # ruta principal
+def index():
+    return render_template('login.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
